795c615715903435c9e0d2f3ccd89aa21198b196
sk <shadaab@saertechnologies.com>
2023-08-26 12:26:24 +0530

    second commint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 export default function page() {
   return (
     <main className={styles.main}>
-      <h1>Service Tasker Website </h1>
-      <p>B2B Solution End To End fro</p>
+      <h1>Service Tasker Website  </h1>
+      <p>B2B Solution End To End for Tasker and Bider For work </p>
       <Link href="/blogs/"style={{background:'black' , padding:'10px' , color:'white'}}>Blogs (SSR)</Link>
       <Link href="/blog/" style={{background:'red' , padding:'10px', marginTop:'10px' , color:'white'}} >Blog  (CSR)</Link>
     </main>
